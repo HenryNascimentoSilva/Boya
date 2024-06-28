b67cd8c2a1c670d9a4419503d72eae91b7d9b543
henrynascimentosilva <henrynascimentosilva@gmail.com>
2024-06-28 17:41:53 +0000

    criando nova branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-import 'groups_screen.dart';
-import 'add_group_screen.dart';
-import 'profile_screen.dart';
+import 'package:myapp/pages/home_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -15,80 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Grupos App',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'Montserrat'
       ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = [
-    const GroupsScreen(),
-    const AddGroupScreen(),
-    const ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: Container(
-        color: Colors.grey[300],
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-            child: GNav(
-              gap: 8,
-              activeColor: Colors.black,
-              iconSize: 24,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              duration: const Duration(milliseconds: 800),
-              tabBackgroundColor: Colors.white,
-              tabBorderRadius: 8,
-              color: Colors.grey,
-              textStyle: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-              ),
-              tabs: const [
-                GButton(
-                  icon: Icons.home,
-                  text: 'Grupos',
-                ),
-                GButton(
-                  icon: Icons.add_circle,
-                  text: 'Adicionar',
-                ),
-                GButton(
-                  icon: Icons.account_circle,
-                  text: 'Perfil',
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: _onItemTapped,
-            ),
-          ),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
